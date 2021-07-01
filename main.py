58b22f7d9acc23e4405c996f3f6fc3a0041c9aaf
Alberto Ortiz <alberto.ortiz.vargas@gmail.com>
2021-07-01 13:05:00 -0500

    Creando endpoint para agregar

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 import json
 
 app = FastAPI()
@@ -13,4 +13,17 @@ datos = {
 @app.get("/")
 def raiz():
     sin_codificar = json.dumps(datos)
+    return json.loads(sin_codificar)
+    
+
+@app.post("/agregar")
+async def agregar(request:Request):
+    nuevos_datos = {}
+    formdata = await request.form()
+    i = 1
+    for id in datos:
+        nuevos_datos[str(id)] = datos[id]
+        i+=1
+    nuevos_datos[str(i)] = formdata["nuevolenguaje"]
+    sin_codificar = json.dumps(nuevos_datos)
     return json.loads(sin_codificar)
